e5286d48598f79878e41ae2ca92b7381ce943365
singularitti <singularitti@outlook.com>
2022-12-20 17:41:39 -0500

    Add `discretize_laplacian` function in src/Electrostatics.jl

--- src/Electrostatics.jl
+++ src/Electrostatics.jl
@@ -1,5 +1,18 @@
 module Electrostatics
 
+using LinearAlgebra: Symmetric, diagm
 
+export discretize_laplacian
+
+function discretize_laplacian(N)
+    A = diagm(
+        0 => fill(-4, N^2),
+        1 => fill(1, N^2 - 1),
+        N - 1 => fill(1, N^2 - N + 1),
+        N => fill(1, N^2 - N),
+        N^2 - N => fill(1, N),
+    )  # An upper triangular matrix
+    return collect(Symmetric(A))
+end
 
 end
