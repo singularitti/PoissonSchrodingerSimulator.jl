d783c23639fa25203754c5f404ab67275ae76f14
singularitti <singularitti@outlook.com>
2022-12-19 22:31:59 -0500

    Add types `IterationStep`, `ConvergenceHistory`

--- src/ConjugateGradient.jl
+++ src/ConjugateGradient.jl
@@ -1,5 +1,18 @@
 module ConjugateGradient
 
 
+struct IterationStep
+    n::UInt64
+    alpha::Float64
+    beta::Float64
+    x::Vector{Float64}
+    r::Vector{Float64}
+    p::Vector{Float64}
+end
+
+mutable struct ConvergenceHistory
+    isconverged::Bool
+    history::Vector{IterationStep}
+end
 
 end
