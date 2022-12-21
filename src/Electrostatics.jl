1d2ae6395332bfe52c4c6f624d9c42557962d4ff
singularitti <singularitti@outlook.com>
2022-12-20 22:00:05 -0500

    Add `checkbc`, `checksquare`, `checkcharges`

--- src/Electrostatics.jl
+++ src/Electrostatics.jl
@@ -2,7 +2,16 @@ module Electrostatics
 
 using ..LastHomework: DiscreteLaplacianPBCs
 
-export setbc!, setsquare!, setcharges!
+export checkbc, checksquare, checkcharges, setbc!, setsquare!, setcharges!
+
+function checkbc(ϕ::AbstractMatrix, v)
+    @assert ϕ[begin, :] == v  # Top
+    @assert ϕ[end, :] == v  # Bottom
+    @assert ϕ[:, begin] == v  # Left
+    @assert ϕ[:, end] == v  # Right
+    return nothing
+end
+checkbc(𝛟::AbstractVector, M, N, v) = _checkvector(checkbc, 𝛟, M, N, v)
 
 function setbc!(ϕ::AbstractMatrix, v=zero(eltype(ϕ)))
     ϕ[begin, :] = v  # Top
@@ -16,6 +25,18 @@ function setbc!(𝛟::AbstractVector, M, N, v=zero(eltype(𝛟)))
     ϕ = setbc!(ϕ, v)
     return reshape(ϕ, length(ϕ))
 end
+
+function checksquare(ϕ::AbstractMatrix, v)
+    M, N = size(ϕ)
+    xₘᵢₙ, xₘₐₓ, yₘᵢₙ, yₘₐₓ = map(Int64, (M / 2, M * 3//4, N * 5//8, N * 7//8))
+    for i in xₘᵢₙ:xₘₐₓ
+        for j in yₘᵢₙ:yₘₐₓ
+            @assert ϕ[i, j] == v
+        end
+    end
+    return nothing
+end
+checksquare(𝛟::AbstractVector, M, N, v) = _checkvector(checksquare, 𝛟, M, N, v)
 
 function setsquare!(ϕ::AbstractMatrix, v=oneunit(eltype(ϕ)))
     M, N = size(ϕ)
@@ -33,6 +54,15 @@ function setsquare!(𝛟::AbstractVector, M, N, v=oneunit(eltype(𝛟)))
     return reshape(ϕ, length(ϕ))
 end
 
+function checkcharges(ρ::AbstractMatrix, v)
+    M, N = size(ρ)
+    x₁, x₂, y = map(Int64, (M / 4, M * 3//4, N / 8))
+    @assert ρ[x₁, y] == v
+    @assert ρ[x₂, y] == v
+    return nothing
+end
+checkcharges(𝛒::AbstractVector, M, N, v) = _checkvector(checkcharges, 𝛒, M, N, v)
+
 function setcharges!(ρ::AbstractMatrix, v=oneunit(eltype(ρ)))
     M, N = size(ρ)
     x₁, x₂, y = map(Int64, (M / 4, M * 3//4, N / 8))
@@ -45,5 +75,7 @@ function setcharges!(𝛒::AbstractVector, M, N, v=oneunit(eltype(𝛒)))
     ρ = setsquare!(ρ, v)
     return reshape(ρ, length(ρ))
 end
+
+_checkvector(f::Function, 𝐯::AbstractVector, M, N, value) = f(reshape(𝐯, M, N), value)
 
 end
