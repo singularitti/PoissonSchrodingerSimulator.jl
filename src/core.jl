19937ae19b1304cea90440cff95ac6c6455d4941
singularitti <singularitti@outlook.com>
2022-12-22 18:31:55 -0500

    Fix filling `DiscreteLaplacian`

--- src/core.jl
+++ src/core.jl
@@ -9,8 +9,8 @@ end
 function DiscreteLaplacian(N::Integer)
     A = spdiagm(
         0 => fill(-4, N^2),
-        1 => fill(1, N^2 - 1),
-        N - 1 => fill(1, N^2 - N + 1),
+        1 => [mod(i, N) == N - 1 ? 0 : 1 for i in 0:(N^2 - 2)],
+        N - 1 => [mod(i, N) == 0 ? 1 : 0 for i in 0:(N^2 - N)],
         N => fill(1, N^2 - N),
         N^2 - N => fill(1, N),
     )  # An upper triangular matrix
