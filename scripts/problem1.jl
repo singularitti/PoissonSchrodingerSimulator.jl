78f01a89f6632e33682b71b1280df19d2dffa567
singularitti <singularitti@outlook.com>
2022-12-22 04:27:06 -0500

    Rewrite scripts/problem1.jl

--- scripts/problem1.jl
+++ scripts/problem1.jl
@@ -8,11 +8,14 @@ using LastHomework.Electrostatics
 
 N = 128
 𝛟₀ = SolutionVector(zeros(N^2));
-set!(𝛟₀, Boundary(0))
-set!(𝛟₀, InternalSquare(5))
+set!(𝛟₀, Boundary(0));
+set!(𝛟₀, InternalSquare(5));
 𝛒₀ = ResidualVector(zeros(N^2));
-set!(𝛒₀, PointCharges(-20))
-A = sparse(DiscreteLaplacian(N))
+set!(𝛒₀, PointCharges(-20));
+A = sparse(DiscreteLaplacian(N));
 
-𝛟, history = solve(A, -𝛒₀, 𝛟₀; maxiter=4)
-regionheatmap(reshape(𝛟, N, N))
+𝛟, history = solve(A, -𝛒₀, 𝛟₀; maxiter=1000)
+phimat = collect(reshape(𝛟, N, N))
+regionheatmap(phimat)
+surfaceplot(phimat)
+surfaceplot!(collect(reshape(𝛟₀, N, N)))
