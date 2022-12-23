629108726960fe896aea8ff7933c088666acb2ac
singularitti <singularitti@outlook.com>
2022-12-23 14:01:47 -0500

    Fix `solve!` in src/Electrostatics.jl
    Hooray!

--- src/Electrostatics.jl
+++ src/Electrostatics.jl
@@ -100,26 +100,29 @@ function solve!(
     setvalues!(𝐱₀, BOUNDARY)
     setvalues!(𝐱₀, SQUARE)
     setvalues!(𝐛, PointCharges((N, N), charge))
-    𝐱ₙ = 𝐱₀
+    𝐱ₙ = copy(𝐱₀)
     𝐫ₙ = 𝐛 - A * 𝐱ₙ  # Initial residual, 𝐫₀
-    𝐩ₙ = copy(𝐫ₙ)  # Initial momentum, 𝐩₀
+    𝐩ₙ = 𝐫ₙ  # Initial momentum, 𝐩₀, notice that if you cahnge 𝐩ₙ, 
     for n in 0:maxiter
         if norm(𝐫ₙ) < atol
             setconverged!(logger)
             break
         end
         setvalues!(𝐩ₙ, BOUNDARY)
-        setvalues!(𝐩ₙ, SQUARE)
+        setvalues!(𝐩ₙ, SQUARE_RESIDUAL)  # Set 𝐩ₙ and 𝐫₀ to zero in the square
         A𝐩ₙ = A * 𝐩ₙ  # Avoid running it multiple times
-        setvalues!(A𝐩ₙ, BOUNDARY)
-        setvalues!(A𝐩ₙ, SQUARE)
         αₙ = dot(𝐫ₙ, 𝐫ₙ) / dot(𝐩ₙ, A𝐩ₙ)
         𝐱ₙ₊₁ = 𝐱ₙ + αₙ * 𝐩ₙ
-        𝐫ₙ₊₁ = 𝐫ₙ - αₙ * A𝐩ₙ
+        αₙA𝐩ₙ = αₙ * A𝐩ₙ
+        setvalues!(αₙA𝐩ₙ, BOUNDARY)
+        setvalues!(αₙA𝐩ₙ, SQUARE_RESIDUAL)  # Set αₙA𝐩ₙ to 0 in the square
+        𝐫ₙ₊₁ = 𝐫ₙ - αₙA𝐩ₙ
         βₙ = dot(𝐫ₙ₊₁, 𝐫ₙ₊₁) / dot(𝐫ₙ, 𝐫ₙ)
         𝐩ₙ₊₁ = 𝐫ₙ₊₁ + βₙ * 𝐩ₙ
         log!(logger, IterationStep(n, αₙ, βₙ, 𝐱ₙ, 𝐫ₙ, 𝐩ₙ))
         𝐱ₙ, 𝐫ₙ, 𝐩ₙ = 𝐱ₙ₊₁, 𝐫ₙ₊₁, 𝐩ₙ₊₁  # Prepare for a new iteration
+        # validate(𝐫ₙ, SQUARE_RESIDUAL)
+        # validate(𝐱ₙ, SQUARE)  # Check if 𝐱ₙ is 5 in the square
     end
     return 𝐱ₙ
 end
