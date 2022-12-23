2713bb076574309458adf834e2b5195ca97e1598
singularitti <singularitti@outlook.com>
2022-12-23 02:25:28 -0500

    Recover `getindices` for `AbstractVector` & `AbstractMatrix`
    Looks like #13, which was deprecated in #14

--- src/Electrostatics.jl
+++ src/Electrostatics.jl
@@ -22,8 +22,7 @@ struct PointCharges{T} <: FixedValueRegion{T}
     value::T
 end
 
-function getindices(ϕ, region::Boundary)
-    ϕ = reshape(ϕ, region.boxsize)
+function getindices(ϕ::AbstractMatrix, ::Boundary)
     cartesian_indices = CartesianIndices(ϕ)
     # Note the geometry of the region and the matrix rows/columns ordering are the same!
     # See https://discourse.julialang.org/t/how-to-get-the-cartesian-indices-of-a-row-column-in-a-matrix/91940/2
@@ -34,19 +33,25 @@ function getindices(ϕ, region::Boundary)
         cartesian_indices[:, end],  # Right
     )
 end
-function getindices(ϕ, region::InternalSquare)
-    M, N = region.boxsize .- 1
-    ϕ = reshape(ϕ, region.boxsize)
+function getindices(ϕ::AbstractMatrix, ::InternalSquare)
+    M, N = size(ϕ) .- 1
     xₘᵢₙ, xₘₐₓ, yₘᵢₙ, yₘₐₓ = map(Int64, (M / 2, M * 3//4, N * 5//8, N * 7//8))
     return map(Iterators.product(xₘᵢₙ:xₘₐₓ, yₘᵢₙ:yₘₐₓ)) do (i, j)
         CartesianIndex(j + 1, i + 1)   # Note y -> row, x -> column
     end
 end
-function getindices(ρ, region::PointCharges)
-    M, N = region.boxsize .- 1
-    ρ = reshape(ρ, region.boxsize)
+function getindices(ρ::AbstractMatrix, ::PointCharges)
+    M, N = size(ρ) .- 1
     x₁, x₂, y = map(Int64, (M / 4, M * 3//4, N / 8)) .+ 1
     return map(CartesianIndex, ((y, x₁), (y, x₂)))  # Note y -> row, x -> column
+end
+# See See https://discourse.julialang.org/t/how-to-convert-cartesianindex-n-values-to-int64/15074/4
+# and http://docs.julialang.org/en/v1/base/arrays/#Base.LinearIndices
+function getindices(vec::AbstractVector, region::FixedValueRegion)
+    mat = reshape(vec, region.boxsize)
+    linear_indices = LinearIndices(mat)
+    cartesian_indices = collect(getindices(mat, region))  # `getindex` only accepts vector indices
+    return linear_indices[cartesian_indices]
 end
 
 function checkequal(data::PartiallyFixedMatrix, region::FixedValueRegion)
