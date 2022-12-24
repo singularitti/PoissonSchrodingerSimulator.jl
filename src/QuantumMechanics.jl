b8f2f952bb8dd512cb9ba44537a733dad835ea05
singularitti <singularitti@outlook.com>
2022-12-24 02:53:02 -0500

    Implement array interface for `Hamiltonian`

--- src/QuantumMechanics.jl
+++ src/QuantumMechanics.jl
@@ -13,4 +13,14 @@ end
 Hamiltonian(A::DiscreteLaplacian, 𝛟::AbstractVector, q::Number) =
     Hamiltonian(A[diagind(A)] .+ q * 𝛟)
 
+Base.parent(S::Hamiltonian) = S.parent
+
+Base.size(S::Hamiltonian) = size(parent(S))
+
+Base.IndexStyle(::Type{Hamiltonian{T}}) where {T} = IndexLinear()
+
+Base.getindex(S::Hamiltonian, i) = getindex(parent(S), i)
+
+Base.setindex!(S::Hamiltonian, v, i) = setindex!(parent(S), v, i)
+
 end
