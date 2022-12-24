f1b2de30ee5c716a037706698d4e49f6179f72c8
singularitti <singularitti@outlook.com>
2022-12-24 02:51:49 -0500

    Add type `Hamiltonian` in src/QuantumMechanics.jl

--- src/QuantumMechanics.jl
+++ src/QuantumMechanics.jl
@@ -1,5 +1,16 @@
 module QuantumMechanics
 
+using LinearAlgebra: SymTridiagonal, norm, normalize, diagind
+using SparseArrays: AbstractSparseMatrix, SparseMatrixCSC
 
+using ..LastHomework: DiscreteLaplacian
+
+export Hamiltonian
+
+struct Hamiltonian{T} <: AbstractSparseMatrix{T,Int64}
+    parent::SparseMatrixCSC{T,Int64}
+end
+Hamiltonian(A::DiscreteLaplacian, 𝛟::AbstractVector, q::Number) =
+    Hamiltonian(A[diagind(A)] .+ q * 𝛟)
 
 end
