d0a46060d17fd1a25a90fc2196d8a772d39902b8
singularitti <singularitti@outlook.com>
2022-12-23 23:53:36 -0500

    Move `FixedValueRegion`, `getindices` to src/regions.jl

--- src/Electrostatics.jl
+++ src/Electrostatics.jl
@@ -2,85 +2,11 @@ module Electrostatics
 
 using LinearAlgebra: norm, dot
 
-using ..LastHomework: DiscreteLaplacian
+using ..LastHomework:
+    DiscreteLaplacian, Boundary, InternalSquare, PointCharges, validate, setvalues!
 using ..ConjugateGradient: IterationStep, setconverged!, log!
 
 import ..ConjugateGradient: solve!
-
-export Boundary, InternalSquare, PointCharges, getindices, validate, getvalues, setvalues!
-
-abstract type FixedValueRegion{T} end
-struct Boundary{T} <: FixedValueRegion{T}
-    boxsize::NTuple{2,Int}
-    value::T
-end
-struct InternalSquare{T} <: FixedValueRegion{T}
-    boxsize::NTuple{2,Int}
-    value::T
-end
-struct PointCharge{T} <: FixedValueRegion{T}
-    boxsize::NTuple{2,Int}
-    value::T
-end
-struct PointCharges{T} <: FixedValueRegion{T}
-    boxsize::NTuple{2,Int}
-    value::T
-end
-
-function getindices(ϕ::AbstractMatrix, ::Boundary)
-    cartesian_indices = CartesianIndices(ϕ)
-    # Note the geometry of the region and the matrix rows/columns ordering are the same!
-    # See https://discourse.julialang.org/t/how-to-get-the-cartesian-indices-of-a-row-column-in-a-matrix/91940/2
-    return vcat(
-        cartesian_indices[begin, :],  # Bottom
-        cartesian_indices[end, :],  # Top
-        cartesian_indices[:, begin],  # Left
-        cartesian_indices[:, end],  # Right
-    )
-end
-function getindices(ϕ::AbstractMatrix, ::InternalSquare)
-    M, N = size(ϕ) .- 1
-    xₘᵢₙ, xₘₐₓ, yₘᵢₙ, yₘₐₓ = map(Int64, (M / 2, M * 3//4, N * 5//8, N * 7//8))
-    return map(Iterators.product(xₘᵢₙ:xₘₐₓ, yₘᵢₙ:yₘₐₓ)) do (i, j)
-        CartesianIndex(j + 1, i + 1)   # Note y -> row, x -> column
-    end
-end
-function getindices(ρ::AbstractMatrix, ::PointCharges)
-    M, N = size(ρ) .- 1
-    x₁, x₂, y = map(Int64, (M / 4, M * 3//4, N / 8)) .+ 1
-    return map(CartesianIndex, ((y, x₁), (y, x₂)))  # Note y -> row, x -> column
-end
-# See See https://discourse.julialang.org/t/how-to-convert-cartesianindex-n-values-to-int64/15074/4
-# and http://docs.julialang.org/en/v1/base/arrays/#Base.LinearIndices
-function getindices(vec::AbstractVector, region::FixedValueRegion)
-    mat = reshape(vec, region.boxsize)
-    linear_indices = LinearIndices(mat)
-    cartesian_indices = collect(getindices(mat, region))  # `getindex` only accepts vector indices
-    return linear_indices[cartesian_indices]
-end
-
-function validate(data, region::FixedValueRegion)
-    indices = getindices(data, region)
-    for index in indices
-        @assert data[index] == region.value
-    end
-    return nothing
-end
-
-function getvalues(data, region::FixedValueRegion)
-    indices = getindices(data, region)
-    return map(indices) do index
-        data[index]
-    end
-end
-
-function setvalues!(data, region::FixedValueRegion)
-    indices = getindices(data, region)
-    for index in indices
-        data[index] = region.value
-    end
-    return vec(data)
-end
 
 function solve!(
     logger,
--- src/LastHomework.jl
+++ src/LastHomework.jl
@@ -3,6 +3,7 @@ module LastHomework
 include("core.jl")
 include("ConjugateGradient.jl")
 include("Lanczos.jl")
+include("regions.jl")
 include("Electrostatics.jl")
 include("QuantumMechanics.jl")
 include("plotting.jl")
