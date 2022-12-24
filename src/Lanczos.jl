83b65bf1ea45c956e5c68bc6836de0ea9cdef0f5
singularitti <singularitti@outlook.com>
2022-12-24 03:47:34 -0500

    Fix argument `M` to `maxiter` in `*lanczos` in src/Lanczos.jl

--- src/Lanczos.jl
+++ src/Lanczos.jl
@@ -4,18 +4,18 @@ using LinearAlgebra: SymTridiagonal, norm, normalize, eigen, ⋅
 
 export lanczos, restart_lanczos, loop_lanczos
 
-function lanczos(A::AbstractMatrix, M=size(A, 2), 𝐪₁=normalize(rand(M)), β₁=0)
+function lanczos(A::AbstractMatrix, 𝐪₁=normalize(rand(size(A, 1))), β₁=0; maxiter=30)
     n = 1  # Initial step
     𝐪₁ = normalize(𝐪₁)
-    Q = Matrix{eltype(𝐪₁)}(undef, size(A, 1), M)  # N × M
+    Q = Matrix{eltype(𝐪₁)}(undef, size(A, 1), maxiter)  # N × M
     Q[:, 1] = 𝐪₁
     𝐩₁ = A * 𝐪₁
     α₁ = 𝐪₁ ⋅ 𝐩₁  # 𝐪ₙ⊺ A 𝐪ₙ
     𝐫ₙ = 𝐩₁ - α₁ * 𝐪₁  # 𝐫₁, Gram–Schmidt process
-    𝛂 = Vector{eltype(float(α₁))}(undef, M)
-    𝛃 = Vector{eltype(float(β₁))}(undef, M)
+    𝛂 = Vector{eltype(float(α₁))}(undef, maxiter)
+    𝛃 = Vector{eltype(float(β₁))}(undef, maxiter)
     𝛂[n], 𝛃[n] = α₁, β₁
-    for n in 2:M
+    for n in 2:maxiter
         𝐫ₙ₋₁ = 𝐫ₙ
         𝛃[n] = norm(𝐫ₙ₋₁)
         if iszero(𝛃[n])
@@ -32,21 +32,24 @@ function lanczos(A::AbstractMatrix, M=size(A, 2), 𝐪₁=normalize(rand(M)), β
     return T, Q
 end
 
-function restart_lanczos(A::AbstractMatrix, M=size(A, 2), 𝐪₁=normalize(rand(M)), β₁=0)
-    T, Q = lanczos(A, M, 𝐪₁, β₁)
+function restart_lanczos(
+    A::AbstractMatrix, 𝐪₁=normalize(rand(size(A, 1))), β₁=0; maxiter=30
+)
+    T, Q = lanczos(A, 𝐪₁, β₁; maxiter=maxiter)
     vals, vecs = eigen(T)
     index = argmin(vals)
     𝐰 = vecs[index]
     subspacedim = length(𝐰)
-    @assert subspacedim == M
-    return normalize(vec(𝐰' * Q[axes(𝐰, 1), begin:M]))
+    return normalize(vec(𝐰' * Q[axes(𝐰, 1), begin:subspacedim]))
 end
 
-function loop_lanczos(A::AbstractMatrix, n, M=size(A, 2), 𝐪₁=normalize(rand(M)), β₁=0)
+function loop_lanczos(
+    A::AbstractMatrix, n, M=size(A, 2), 𝐪₁=normalize(rand(size(A, 1))), β₁=0; maxiter=30
+)
     for _ in 1:n
-        𝐪₁ = restart_lanczos(A, M, 𝐪₁, β₁)
+        𝐪₁ = restart_lanczos(A, 𝐪₁, β₁; maxiter=maxiter)
     end
-    return lanczos(A, M, 𝐪₁, β₁)  # Do one last Lanczos
+    return lanczos(A, 𝐪₁, β₁; maxiter=maxiter)  # Do one last Lanczos
 end
 
 end
